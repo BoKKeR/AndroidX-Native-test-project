1d271ed340f2baae63d00d90306b1aabd4f3d6f0
Norbert Takács <bokker11@hotmail.com>
2019-10-19 13:13:18 +0200

    4 tabs

--- otto/app/src/main/java/com/example/otto/ui/jobs/JobsFragment.java
+++ otto/app/src/main/java/com/example/otto/ui/jobs/JobsFragment.java
@@ -4,12 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.otto.R;
@@ -23,13 +19,6 @@ public class JobsFragment extends Fragment {
         jobsViewModel =
                 ViewModelProviders.of(this).get(JobsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_jobs, container, false);
-        final TextView textView = root.findViewById(R.id.text_jobs);
-        jobsViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
     }
 }
