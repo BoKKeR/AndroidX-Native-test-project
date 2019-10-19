07ffe9b07e5dec2bbbca7b3e320b6a3ef2494acc
Norbert Takács <bokker11@hotmail.com>
2019-10-19 16:02:21 +0200

    dao

---
+++ otto/app/src/main/java/com/example/otto/JobDao.java
@@ -0,0 +1,26 @@
+package com.example.otto;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface JobDao {
+
+    @Insert
+    void insert(Job job);
+
+    @Update
+    void update(Job job);
+
+    @Delete
+    void delete(Job job);
+
+    @Query("SELECT * FROM job_table ORDER BY priority DESC")
+    LiveData<List<Job>> getAllJobs();
+}
